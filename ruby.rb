1364b87a90c88b102b112cbb83ce540a7268822e
Fred Morlock <FrederickGeek8@gmail.com>
2013-01-08 06:20:06 -0500

    Added Ruby example

---
+++ ruby.rb
@@ -0,0 +1,8 @@
+puts "puts works"
+puts " with line breaks."
+
+print "print works"
+print " with no line breaks."
+
+printf("\n\nprintf formats numbers like %7.2f, and
+strings like %s.",3.14156,"me")
