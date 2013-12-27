d1843018618685a8abc8cca8840ab9611ac35349
Wasim Sandhu <peacemakerdawg@me.com>
2013-12-27 01:14:52 -0800

    Added Javascript example
    Hope I didn’t forget anything. I’ll add more to this.

---
+++ javascript.js
@@ -0,0 +1,16 @@
+// This is a comment and will ultimately be ignored.
+window.prompt("Hello World!");
+// Below, we have created a variable called myAge and set it to 5.
+var myAge = 15;
+// Here's a basic if/else statement:
+if (myAge == 15) {
+    console.log("Dude, you're fifteen.");
+}
+else if (var < 5) {
+    console.log("Coochie coochie coo!");
+}
+else {
+    console.log("You're old, mate.");
+}
+// Below is how to find the length of strings.
+"How old am I???".length
